dc089a804b25a4b8d22822eea30ced12174e6fcd
kαℓүαη 𓃠 <im.kalyan00@gmail.com>
2026-03-17 10:10:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,94 +1,58 @@
-// background stars
+function goBack(){
 
-let canvas=document.getElementById("bg")
-let ctx=canvas.getContext("2d")
+let voice=new SpeechSynthesisUtterance("Bye Bye")
 
-canvas.width=window.innerWidth
-canvas.height=window.innerHeight
-
-let stars=[]
-
-for(let i=0;i<200;i++){
-stars.push({
-x:Math.random()*canvas.width,
-y:Math.random()*canvas.height
-})
-}
-
-function drawStars(){
-ctx.clearRect(0,0,canvas.width,canvas.height)
-
-for(let s of stars){
-ctx.fillStyle="white"
-ctx.fillRect(s.x,s.y,2,2)
-
-s.y+=0.5
-if(s.y>canvas.height) s.y=0
-}
-
-requestAnimationFrame(drawStars)
-}
-
-drawStars()
-
-// milkyway warp
-
-function enterUniverse(){
-
-let warp=document.createElement("div")
-
-warp.style.position="fixed"
-warp.style.top="0"
-warp.style.left="0"
-warp.style.width="100%"
-warp.style.height="100%"
-warp.style.background="black"
-warp.style.zIndex="9999"
-
-document.body.appendChild(warp)
-
-let c=document.createElement("canvas")
-c.width=window.innerWidth
-c.height=window.innerHeight
-warp.appendChild(c)
-
-let ctx2=c.getContext("2d")
-
-let stars=[]
-
-for(let i=0;i<400;i++){
-stars.push({
-x:Math.random()*c.width,
-y:Math.random()*c.height,
-z:Math.random()*c.width
-})
-}
-
-function animate(){
-ctx2.fillStyle="black"
-ctx2.fillRect(0,0,c.width,c.height)
-
-for(let s of stars){
-
-s.z-=15
-if(s.z<=0) s.z=c.width
-
-let k=128/s.z
-let px=s.x*k+c.width/2
-let py=s.y*k+c.height/2
-
-ctx2.fillStyle="white"
-ctx2.fillRect(px,py,2,2)
-
-}
-
-requestAnimationFrame(animate)
-}
-
-animate()
+speechSynthesis.speak(voice)
 
 setTimeout(()=>{
-window.location="space.html"
+
+location.href="index.html"
+
+},1000)
+
+}
+
+function magic(){
+
+for(let i=0;i<80;i++){
+
+let star=document.createElement("div")
+
+star.innerHTML="✨"
+
+star.style.position="fixed"
+
+star.style.left=Math.random()*100+"%"
+
+star.style.top=Math.random()*100+"%"
+
+star.style.fontSize="30px"
+
+star.style.transition="all 2s"
+
+document.body.appendChild(star)
+
+setTimeout(()=>{
+
+star.style.transform="scale(0)"
+star.style.opacity="0"
+
+},100)
+
+}
+
+let rings=document.querySelectorAll(".ring1,.ring2")
+
+rings.forEach(r=>{
+
+r.style.boxShadow="0 0 100px cyan,0 0 200px magenta"
+
+setTimeout(()=>{
+
+r.style.boxShadow="0 0 20px cyan"
+
 },2000)
+
+})
 
 }
